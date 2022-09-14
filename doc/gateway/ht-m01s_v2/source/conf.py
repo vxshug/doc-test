00b0b8e9cc1b0863e70b9e33c86f5a799ffbd4dc
vxzyfx <vxzyfx@gmail.com>
2022-09-15 01:30:47 +0800

    csjkaxbjs

--- doc/gateway/ht-m01s_v2/source/conf.py
+++ doc/gateway/ht-m01s_v2/source/conf.py
@@ -14,6 +14,8 @@ release = 'latest'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
 sys.path.insert(0, os.path.abspath('../../../_ext/'))
 from common_conf import *
 
