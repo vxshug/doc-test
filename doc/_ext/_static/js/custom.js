3dfb6c25b33ce9b660b0e80aabd7d019e0934245
vxzyfx <vxzyfx@gmail.com>
2022-09-15 16:54:20 +0800

    sabxjsja

--- doc/_ext/_static/js/custom.js
+++ doc/_ext/_static/js/custom.js
@@ -16,19 +16,19 @@ var prod_obj = {
     ],
     node: [
         {text: "CUBECELL", value: "cubecell"},
-        {text: "stm32", value: "stm32"},
+        {text: "STM32", value: "stm32"},
+        {text: "ESP32", value: "esp32"},
+        {text: "ESP32+ARDUINO", value: "esp32_arduino"},
+        {text: "ESP8266+ARDUINO", value: "esp8266_arduino"},
     ],
 }
 
 function tt() {
     let e = document.getElementById("product-select");
     let t = $("#type-select").val();
-    console.log(t);
-    console.log(prod_obj);
     e.options.length = 1
     for (let r = 0; r < prod_obj[t].length; r++) {
         let m = prod_obj[t][r];
-        console.log(m);
         var o = new Option(m.text,m.value);
         e.add(o)
     }
@@ -39,11 +39,9 @@ function tt() {
     window.location.href = function(e) {
         let t = $("#type-select").val();
         let href = window.location.href.split('/')
-        console.log(href)
         let ll = document.documentElement.lang.replace('-', '_')
         let l = href.indexOf(ll) + 1
         href.splice(l, href.length)
-        console.log(href.join('/') + '/' + t + '/' + e + '/index.html')
         return href.join('/') + '/' + t + '/' + e + '/index.html'
     }(n)
 }
@@ -53,10 +51,6 @@ var t = document.getElementById("type-select");
 var e = document.getElementById("product-select");
 var th = window.location.href.split('/')[4]
 var eh = window.location.href.split('/')[5]
-console.log(t)
-console.log(e)
-console.log(th)
-console.log(eh)
 t.hidden = !1;
 t.onchange = tt;
 e.hidden = !1;
